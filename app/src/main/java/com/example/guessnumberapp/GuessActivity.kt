43695b8e181cb3124cc0d365b7b55d3661934d84
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-05-22 21:01:02 +0300

    edit

--- app/src/main/java/com/example/guessnumberapp/GuessActivity.kt
+++ app/src/main/java/com/example/guessnumberapp/GuessActivity.kt
@@ -3,21 +3,53 @@ package com.example.guessnumberapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.guessnumberapp.databinding.ActivityGuessBinding
 import com.example.guessnumberapp.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class GuessActivity : AppCompatActivity() {
         lateinit var binding: ActivityGuessBinding
 
+        private var randomNumber = 0
+        private var count = 5
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGuessBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonGuess.setOnClickListener{
-            finish()
-            startActivity(Intent(this@GuessActivity,ResultActivity::class.java))
+        randomNumber = Random.nextInt(101)//0 with 101
 
+        Log.e("Rastgele Sayı", randomNumber.toString())
+
+        binding.buttonGuess.setOnClickListener{
+
+
+            val myNumber = binding.editTextGuess.text.toString().toInt()
+
+            count = count - 1
+
+            if (randomNumber == myNumber){
+                finish()
+                startActivity(Intent(this@GuessActivity,ResultActivity::class.java))
+                intent.putExtra("result",true)
+                return@setOnClickListener
+            }
+            if (randomNumber > myNumber){
+                binding.textViewTry.text = "Increase"
+                binding.textViewCount.text = "Remaining Rights: :$count"
+            }
+            if (randomNumber < myNumber){
+                binding.textViewTry.text = "Decrease"
+                binding.textViewCount.text = "Remaining Rights: :$count"
+            }
+            if (count == 0){
+                finish()
+                startActivity(Intent(this@GuessActivity,ResultActivity::class.java))
+                intent.putExtra("result",false)
+            }
+            binding.textViewTry.setText("")
         }
+
     }
 }
--- app/src/main/java/com/example/guessnumberapp/ResultActivity.kt
+++ app/src/main/java/com/example/guessnumberapp/ResultActivity.kt
@@ -14,6 +14,17 @@ class ResultActivity : AppCompatActivity() {
         binding = ActivityResultBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val result = intent.getBooleanExtra("result",true)
+
+        if (result){
+            binding.imageViewResult.setImageResource(R.drawable.happy)
+            binding.textViewResult.text = "YOU WON"
+        }
+        else{
+            binding.imageViewResult.setImageResource(R.drawable.baseline_sentiment_very_dissatisfied_24)
+            binding.textViewResult.text = "YOU LOST"
+        }
+
         binding.buttonTry.setOnClickListener{
             finish()
             startActivity(Intent(this@ResultActivity,MainActivity::class.java))
